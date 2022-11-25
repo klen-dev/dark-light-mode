63f3375aa7db023d10a7c4767471662af9527d33
klen_dev <klenmrx@gmail.com>
2022-11-25 23:39:50 +0500

    push

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ let number = document.getElementById("number");
 let numberBtn = document.getElementById('number-btn');
 let natija = document.getElementById("natija");
 let reset = document.getElementById("reset");
+let section = document.getElementById("section");
+let qolgan = document.getElementById("qolgan");
+
 
 darkMode.addEventListener("click" , function(){
     document.body.classList.toggle('dark');
@@ -19,29 +22,44 @@ darkMode.addEventListener("click" , function(){
 toogle.addEventListener("click", ()=>{
     navbarTwo.classList.toggle("navbar-2-active");
     navbar.classList.toggle('navbar-active');
+    section.classList.toggle("section");
 });
 
 
 
 let randomNumber = Math.trunc(Math.random()*20+1);
 let score = 10;
-
+qolgan.textContent = 10;
 
 numberBtn.addEventListener("click", ()=>{
-    let son = number.value*1
-    // console.log(son,randomNumber);
-    if(son == randomNumber){
-        natija.textContent = "Yutdingiz! ";
-        body.style.backgroundColor = "rgb(6, 229, 6)";
+    let son = number.value*1;
+    console.log(son,randomNumber,score);
+    if(score > 0){
+        if(son == randomNumber){
+            natija.textContent = "Yutdingiz! ";
+            body.style.backgroundColor = "rgb(6, 229, 6)";
+            reset.style.display = "block";
+            numberBtn.disabled = true;
+        } else if(son > randomNumber){
+            score--
+            natija.textContent = "Siz kiritgan son katta kichikroq son kiriting! ";
+            qolgan.textContent = score;
+        } else{
+            score--
+            natija.textContent = "Siz kiritgan son kichik, kattaroq son kiriting! ";
+            qolgan.textContent = score;
+        }
+    }else{
+        natija.textContent = "Afsuski yutqazdingiz!";
+        body.style.backgroundColor = "red";
         reset.style.display = "block";
-    } else if(son > randomNumber){
-        natija.textContent = "Siz kiritgan son katta kichikroq son kiriting! ";
-    } else{
-        natija.textContent = "Siz kiritgan son kichik, kattaroq son kiriting! "
+        numberBtn.disabled = true;
+        qolgan.textContent = "";
     }
 });
 console.log(randomNumber, number);
 
 reset.onclick = function(){
     window.location.reload();
+    score+=10;
 };
